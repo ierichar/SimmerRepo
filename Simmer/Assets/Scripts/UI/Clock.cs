02a02a1793ad9fc92d6f60fa43095e977170d0f7
yli530 <yli530@ucsc.edu>
2022-01-22 19:42:15 -0800

    make timer loop and is able to hide and show itself

--- Simmer/Assets/Scripts/UI/Clock.cs
+++ Simmer/Assets/Scripts/UI/Clock.cs
@@ -8,16 +8,18 @@ namespace Simmer.UI
 {
     public class Clock : MonoBehaviour 
     {
+        //this is a temp variable, dont need total time after
         public float totalTime;
-
         public Image fillBar;
+        public Camera cam;
 
-        void Start() {
-            StartCoroutine(SetTimer(totalTime, timerFinished));
+        void Start() 
+        {
+            StartCoroutine(setTimer(totalTime, timerFinished));
         }
 
-        //These are so that you can use in another function, that way you can
-        public IEnumerator SetTimer(float time, Action<float> action) {
+        public IEnumerator setTimer(float time, Action<float> action) 
+        {
             float normTime = 0f;
             while(normTime <= 1f) {
                 fillBar.fillAmount = normTime;
@@ -27,8 +29,42 @@ namespace Simmer.UI
             action(time);
         }
 
-        public void timerFinished(float time) {
+        public void timerFinished(float time) 
+        {
             Debug.Log(time + " seconds has passed!");
+            hideClock();
+            StartCoroutine(wait());
+        }
+
+        //only useful if we want to move the clock
+        public void updatePosition(GameObject target) {
+            transform.position = cam.WorldToScreenPoint(target.transform.position);
+        }
+
+        private void hideClock() 
+        {
+            Image[] clockImage = GetComponentsInChildren<Image>();
+            foreach(Image img in clockImage) 
+            {
+                img.enabled = false;
+            }
+        }
+
+        private void showClock() 
+        {
+            Image[] clockImage = GetComponentsInChildren<Image>();
+            foreach(Image img in clockImage) 
+            {
+                img.enabled = true;
+            }
+        }
+
+        //random functions that just re-enables the clock for testing
+        private IEnumerator wait() 
+        {
+            yield return new WaitForSeconds(2f);
+            showClock();
+            StartCoroutine(setTimer(10f, timerFinished));
         }
     }
 }
