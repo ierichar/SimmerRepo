5d06f9bf59c295e6d518f71ce54d1d47d38d96cd
TheUnaverageJoe <jobrcart@ucsc.edu>
2022-05-02 21:50:06 -0700

    Time Manager changes for forced sleep

--- Simmer/Assets/Scripts/GameManagers/TimeManager.cs
+++ Simmer/Assets/Scripts/GameManagers/TimeManager.cs
@@ -15,7 +15,7 @@ namespace Simmer.CustomTime{
         public static int Hour { get; private set; }
         public static bool AM { get; private set; }
         public static int Day { get; private set; }
-
+        public static bool Paused { get; private set; }
 
         private float minuteToRealTime = 0.07f;
         private float timer;
@@ -34,15 +34,27 @@ namespace Simmer.CustomTime{
             }else{
                 Debug.LogError("AM was not equal to 0 or 1");
             }
+            //@@MPerez132 @@TheUnaverageJoe 5/2/2022
+            // added pause feature ----------------------------
+            if(GlobalPlayerData.currentTime[4]==0){
+                Paused = true;
+            }else if(GlobalPlayerData.currentTime[4]==1){
+                Paused = false;
+            }else{
+                Debug.LogError("AM was not equal to 0 or 1");
+            }
+            // -------------------------------------------------
             //Minute = 0;
             //Hour = 6;
             timer = minuteToRealTime;
+            
             //AM = true;
         }
 
         // Update is called once per frame
         void FixedUpdate()
         {
+            if(Paused) return;
             timer -= Time.deltaTime;
             if(timer <= 0) {
                 Minute++;
@@ -65,5 +77,32 @@ namespace Simmer.CustomTime{
                 timer = minuteToRealTime;
             }
         }
+
+        //@@MPerez132 @@TheUnaverageJoe 5/2/2022
+        //-------------------------------------------
+        public void SetPause(bool shouldPause){
+            if(shouldPause){
+                Paused = true;
+            }else{
+                Paused = false;
+            }
+        }
+
+        //optional parameters are those which have an "=" following the variable
+        // Design choice, dont pass in day as it is implied we only ever would want to go to the next day
+        public void SetTime(int hour=8, int minute=0, bool am=true, bool paused=false){
+            //following if statements enable the ability to implicitly increment 
+            //  the day under the assumtion we cant go back in time
+            if(hour < Hour){
+                Day++;
+            }
+            Hour = hour;
+            Minute = minute;
+            AM = am;
+            //give users the choice to pause but really doesnt make sense in most cases
+            Paused = paused;
+        }
+        //-------------------------------------------
+
     }
 }
